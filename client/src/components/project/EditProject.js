b3baf3c7113700538a6f571e392246758005bd0e
Sanjay Dookhoo <sanjaydookhoo@msn.com>
2020-10-18 21:57:07 -0400

    general idea of CRUD for frontend established

--- client/src/components/project/EditProject.js
+++ client/src/components/project/EditProject.js
@@ -1,13 +1,98 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
-class EditProjects extends Component {
+class EditProject extends Component {
+  state = {
+    name: '',
+    description: '',
+    status: 0
+  }
+  
+  componentDidMount = () => {
+    if(this.props.project){
+      const { name, description, status } = this.props.project
+
+      this.setState({
+        name,
+        description,
+        status
+      })
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value
+    })
+  }
+
+  handleSave = (e) => {
+    e.preventDefault();
+
+    const { name, description, status} = this.state
+    let self = this //The callback in the axios function is called not from within your function, so the 'this' is not pointing to what you expect, i.e., your class.
+
+    if(self.props.project){ //it will be a update
+      axios.put(process.env.REACT_APP_API_URL + '/project', {
+        id: self.props.project.id,
+        name,
+        description,
+        status
+      })
+      .then(function (response) {
+        if(response.status == 200){
+          self.props.closeEdit(response.data)
+        }
+      })
+    }else{ //it will be create
+      axios.post(process.env.REACT_APP_API_URL + '/project', {
+        name,
+        description,
+        status
+      })
+      .then(function (response) {
+        if(response.status == 200){
+          self.props.closeEdit(response.data)
+        }
+      })
+    }
+    
+  }
+
   render() {
-    return (
-        <div>
-          EditProjects
+    const { name, description, status } = this.state
+    const button = this.props.project ? (
+      <a className="waves-effect waves-light btn" onClick={this.handleSave}><i className="material-icons left">save</i>Save</a>
+    ) : (
+      <a className="waves-effect waves-light btn" onClick={this.handleSave}><i className="material-icons left">create</i>Create</a>
+    )
+
+    return(
+      <div className="post card"> 
+        <div className="card-content"> 
+        <form onSubmit={this.handleSave}>
+          <div className="input-field">
+            <input id="name" type="text" className="validate" value={name} onChange={this.handleChange}/>
+            <label htmlFor="name" className="active">Name</label>
+          </div>
+          <div className="input-field">
+            <input id="description" type="text" className="validate" value={description} onChange={this.handleChange}/>
+            <label htmlFor="description" className="active">Description</label>
+          </div>
+          <div className="input-field">
+            <input id="status" type="number" min="0" max="100" className="validate" value={status} onChange={this.handleChange}/>
+            <label htmlFor="status" className="active">Status</label>
+          </div>
+        </form>
         </div>
-    );
+        <div className="card-action">
+          <div className="center">
+            {button}
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
-export default EditProjects;
+export default EditProject;
--- client/src/components/project/ViewProjects.js
+++ client/src/components/project/ViewProjects.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import axios from 'axios'
+import { withRouter } from 'react-router-dom'
+import EditProject from './EditProject';
 
 class ViewProjects extends Component {
   state = {
     projects: [],
-    selected: -1
+    selected: -1,
+    edit: false,
+    create: false
   }
   
   componentDidMount = () => {
@@ -20,21 +24,70 @@ class ViewProjects extends Component {
     })
   }
 
-  handleEditClick = (id) => {
+  handleModifyClick = (id) => {
     this.setState({
       selected: id
     })
   }
 
+  handleEditClick = (id) => {
+    this.setState({
+      edit: true
+    })
+  }
+
   handleCreateClick = () => {
-    console.log("create")
+    this.setState({
+      create: true
+    })
+  }
+
+  closeEdit = (rec_project) => {
+    if (this.state.projects.find(project => project.id == rec_project.id)){ //was a update
+      const newProjects = this.state.projects.filter(project => project.id != rec_project.id)
+
+      this.setState({
+        edit: false,
+        projects: [...newProjects,rec_project]
+      })
+    }else{//was new
+      this.setState({
+        edit: false,
+        projects: [...this.state.projects,rec_project]
+      })
+    }
+    
+  }
+
+  handleDeleteClick = (id) => {
+    let self = this //The callback in the axios function is called not from within your function, so the 'this' is not pointing to what you expect, i.e., your class.
+
+    axios.delete(process.env.REACT_APP_API_URL + '/project', {
+      params: {
+        id
+      }
+    })
+    .then(function (response) {
+      if(response.status == 200){
+        self.setState({
+          projects: self.state.projects.filter(project => project.id != id),
+          selected: -1
+        })
+      }
+    })
+  }
+
+  handleStartManagementClick = (id) => {
+    this.props.history.push('/project/risks')
+
+    /*need to set project selected in redux */
   }
 
   renderTableData() {
     return this.state.projects.map((project, index) => {
        const { id, name, description, status } = project
        return (
-          <tr key={id} onClick={() => this.handleEditClick(id)}>
+          <tr key={id} onClick={() => this.handleModifyClick(id)}>
              <td>{name}</td>
              <td>{description}</td>
              <td> <div className="center"> {status} % </div>
@@ -52,42 +105,59 @@ class ViewProjects extends Component {
     let selected = this.state.selected;
     let projects = this.state.projects;
 
+    const editComponent = this.state.edit ? <EditProject project={this.state.projects.find(project => project.id==this.state.selected)} closeEdit={this.closeEdit}/> : null
+    const createComponent = this.state.create ? <EditProject closeEdit={this.closeEdit}/> : null
+
     if(selected != -1){
       ret = (
-        <div className="post card"> 
-          <div className="card-content"> 
-            <h5> {projects.find(project => project.id == selected).name} </h5>
+        <div>
+          <div className="post card"> 
+            <div className="card-content"> 
+              <h5> {projects.find(project => project.id == selected).name} </h5>
+            </div>
+            <div className="card-action">
+              <a className="waves-effect waves-light btn green" onClick={() => this.handleEditClick(selected)}><i className="material-icons left">edit</i>Edit</a>
+              <a className="waves-effect waves-light btn red" onClick={() => this.handleDeleteClick(selected)}><i className="material-icons left">delete</i>Delete</a>
+              <a className="waves-effect waves-light btn blue" onClick={() => this.handleStartManagementClick(selected)}><i className="material-icons left">insert_chart</i>Start Management</a>
+            </div>
           </div>
+          {editComponent}
         </div>
       )
     }else{
       ret = this.state.projects.length ? (
-        <div className="post card"> 
-          <div className="card-content"> 
-            <table className="highlight" >
-                <thead>
-                  <tr>
-                    <th>Name</th>
-                    <th>Description</th>
-                    <th>Status</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {this.renderTableData()}
-                </tbody>
-            </table>
-          </div>
-          <div className="card-action">
-            <div className="center">
-              <a className="waves-effect waves-light btn" onClick={this.handleCreateClick}><i className="material-icons left">add</i>Create New</a>
+        <div>
+          <div className="post card"> 
+            <div className="card-content"> 
+              <table className="highlight" >
+                  <thead>
+                    <tr>
+                      <th>Name</th>
+                      <th>Description</th>
+                      <th>Status</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {this.renderTableData()}
+                  </tbody>
+              </table>
+            </div>
+            <div className="card-action">
+              <div className="center">
+                <a className="waves-effect waves-light btn" onClick={this.handleCreateClick}><i className="material-icons left">add</i>Create New</a>
+              </div>
             </div>
           </div>
+          {createComponent}
         </div>
       ) : (
-        <div className="post card"> 
-          <div className="card-content"> 
-            No Projects 
+        <div>
+          <div className="post card"> 
+            <div className="card-content"> 
+              No Projects 
+            </div>
           </div>
+          {createComponent}
         </div>
       )
     }
@@ -96,4 +166,4 @@ class ViewProjects extends Component {
   }
 }
 
-export default ViewProjects;
+export default withRouter( ViewProjects );
